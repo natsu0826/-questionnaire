cd506fe8d7dae4dce6406e0d2be49f21c0268168
natsu0826 <hosozawa08@gmail.com>
2023-07-11 10:50:11 +0900

    com

---
+++ app.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mysql = require("mysql2");
+const port = 3000;
+
+app.use(express.static("views"));
+app.set("view engine", "ejs");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "aq_db",
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/create", (req, res) => {
+  const item = req.query.item;
+  res.render("form", { item: item, errors: [] });
+});
+
+
+
+app.post("/submit", (req, res) => {
+  const { name, namekana, gender, email, addres, tel, reason, inquiry } = req.body;
+
+
+  const sql =
+    "INSERT INTO userlist (name, namekana, gender, email, addres, tel, reason, inquiry) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  const values = [name, namekana, gender, email, addres, tel, reason, inquiry];
+
+  con.query(sql, values, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+
+    res.redirect("/edit");
+  });
+});
+
+app.get("/edit", (req, res) => {
+  res.render("edit");
+});
+
+
+app.get("/", (req, res) => {
+  res.redirect("/create");
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
